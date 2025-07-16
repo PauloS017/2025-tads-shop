a5888cb9176c9f2581c547a7bda8a990e8c3695f
PauloS017 <paulo.silva19@estudante.ifms.edu.br>
2025-07-15 21:54:50 -0300

    Aula15/07

--- app/cadastro/marcas/columns.tsx
+++ app/cadastro/marcas/columns.tsx
@@ -4,35 +4,42 @@ import { DeleteButton } from "@/components/delete-button";
 import { Button } from "@/components/ui/button";
 import { Marca } from "@/models/marca"
 import { ColumnDef } from "@tanstack/react-table"
-import {  Edit } from "lucide-react";
+import { Edit } from "lucide-react";
 import Link from "next/link";
 
 export const columns: ColumnDef<Marca>[] = [
   {
     accessorKey: "id",
-    header: "ID",
+    header: () => <div className="w-8">ID</div>,
+    cell: ({ row }) => {
+      return (
+        <div className="">
+          {row.getValue("id")}
+        </div>
+      )
+    }
   },
   {
     accessorKey: "nome",
     header: "Nome",
   },
-   {
-    id:"actions",
+  {
+    id: "actions",
     header: "Ações",
-    cell:({row})=>{
-        const marca = row.original;
-        return(
-            <>
-             <div className="flex gap-2">
-                  <Button size="icon" asChild>
-                    <Link href={`/cadastro/marcas/editar/${marca.id}`}><Edit/></Link>
-                  </Button>
-                  <DeleteButton id={marca.id} />
-                </div>
-            </>
-        )
+    cell: ({ row }) => {
+      const marca = row.original;
+      return (
+        <>
+          <div className="flex gap-2">
+            <Button size="icon" asChild>
+              <Link href={`/cadastro/marcas/editar/${marca.id}`}><Edit /></Link>
+            </Button>
+            <DeleteButton id={marca.id} />
+          </div>
+        </>
+      )
     }
   }
 
-  
+
 ]
